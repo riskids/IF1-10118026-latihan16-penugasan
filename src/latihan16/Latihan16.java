1f1ae86c9ea87faeb5ed254d6ef43a5392af3430
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:47:48 +0700

    Add files via upload

---
+++ src/latihan16/Latihan16.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan16;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar penugasan
+ * 
+ */
+public class Latihan16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = "+a);
+        System.out.println("Nilai b = "+b);
+        System.out.println("Nilai c = "+c);
+        
+        c = a+b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c + = a= " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        
+        c /= a;
+        System.out.println("c/=" +c);
+        
+        a = 10;
+        c = 15;
+        
+        c %= a;
+        System.out.println("c %= = "+c);
+        c <<= 2;
+        System.out.println("c <<= 2 = "+c);
+        c >>= 2;
+        System.out.println("c >>= 2 = "+c);
+        c >>= 2;
+        System.out.println("c >>= 2 = "+c);
+        c &= a;
+        System.out.println("c &= a = "+c);
+        c ^= a;
+        System.out.println("c %= a = "+c);
+        c |= a;
+        System.out.println(" c |= a = "+c);
+        
+    }
+    
+}
